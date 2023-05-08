458038a02ff13e1b792bf13549ed29d295bec9b8
lg0killer <schnetlerlouis@gmail.com>
2023-05-08 12:07:55 +0200

    Updated base skeleton

---
+++ PhotographyCMS/resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,10 @@
+<template>
+  <Link href="/">Main Page</Link>&nbsp;
+  <Link href="/hello">Hello Page</Link>
+
+  <slot></slot>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
---
+++ PhotographyCMS/resources/js/Pages/Index/Index.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Index</h1>
+  <text>The message is: {{ message }}</text>
+</template>
+
+<script setup>
+  defineProps({
+    message: String
+  })
+</script>
