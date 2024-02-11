ca8074ca04aac74913cadc5e5bc38cb170dd0acb
Louigie <schnetlerlouis@gmail.com>
2024-02-11 08:13:11 +0100

    Fix warnings in image box (#15)

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="flex gap-1 py-2">
-    <Link
+    <div v-for="(link, index) in links" :key="index" class="py-2 px-2 rounded-md">
+      <div v-if = "link.url">
+        <Link
+          :is="link.url ? 'a' : 'span'"
+          :href="link.url"
+          :class="{'text-gray-300 dark:text-gray-400 cursor-default': ! link.url, 'font-bold text-gray-300 dark:text-gray-400 cursor-default': link.active}"
+          v-html="link.label"
+        />
+      </div>
+    </div>
+
+    <!-- <Link
       :is="link.url ? 'a' : 'span'"
       v-for="(link, index) in links"
       :key="index" class="py-2 px-2 rounded-md" 
       :href="link.url"
       :class="{'text-gray-300 dark:text-gray-400 cursor-default': ! link.url, 'font-bold text-gray-300 dark:text-gray-400 cursor-default': link.active}"
       v-html="link.label"
-    />
+    /> -->
   </div>
 
 </template>
